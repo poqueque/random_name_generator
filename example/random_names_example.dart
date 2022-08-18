d59cea85210208da8a781f2443453e84e77defea
Edi <poqueque@yahoo.es>
2022-08-18 16:59:48 +0200

    Added examples for all Zones

--- example/random_names_example.dart
+++ example/random_names_example.dart
@@ -2,9 +2,13 @@ import 'package:random_names/random_names.dart';
 
 void main() {
   var randomNames = RandomNames(Zone.france);
-  print("France: ${randomNames.fullName()}");
+  print("France:    ${randomNames.fullName()}");
   randomNames = RandomNames(Zone.us);
-  print("US:     ${randomNames.fullName()}");
+  print("US:        ${randomNames.fullName()}");
+  randomNames = RandomNames(Zone.uk);
+  print("UK:        ${randomNames.fullName()}");
   randomNames = RandomNames(Zone.spain);
-  print("Spain:  ${randomNames.fullName()}");
+  print("Spain:     ${randomNames.fullName()}");
+  randomNames = RandomNames(Zone.catalonia);
+  print("Catalonia: ${randomNames.fullName()}");
 }
